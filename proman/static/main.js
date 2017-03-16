501e0f4d2761759686455fd164c02a44577fe166
Péter Soltész <peter.soltesz92@gmail.com>
2017-03-16 14:10:17 +0100

    updated layout; needs fixing though

--- proman/static/main.js
+++ proman/static/main.js
@@ -1,7 +1,23 @@
-$( function() {
-    $( "#status-field" ).sortable();
-    $( "#status-field" ).disableSelection();
-  } );
+$(document).ready(function () {
+    $('.column .movable-header').mouseenter(function () {
+        $(this).animate({
+            height: '+=10px',
+            width: '+=5px'
+        }, "fast");
+    });
+    $('.column .movable-header').mouseleave(function () {
+        $(this).animate({
+            height: '-=10px',
+            width: '-=5px'
+        }, "fast");
+    });
+});
+
+
+$(function () {
+    $(".column .clmn-content").sortable();
+    $(".column .clmn-content").disableSelection();
+});
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -14,32 +30,4 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-}
-/*$('.card_content, .card_title').click(function(){
-  $(this).toggleClass('clicked');
-}*/
-
-$(document).ready(function () {
-    $('.card-status .movable-header').mouseenter(function () {
-        $(this).animate({
-            height: '+=10px',
-            width: '+=5px'
-        }, "fast");
-    });
-    $('.card-status .movable-header').mouseleave(function () {
-        $(this).animate({
-            height: '-=10px',
-            width: '-=5px'
-        }, "fast");
-    });
-});
-
-class Board {
-    constructor(number, column1, column2, column3, column4) {
-        this.number = number;
-        this.column1 = column1;
-        this.column2 = column2;
-        this.column3 = column3;
-        this.column4 = column4;
-    }
 }
