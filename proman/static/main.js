1b439f94e28c8cdd396204bcb14c6785984d9cf9
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-03-16 11:07:53 +0100

    added status fields and draggable cards

--- proman/static/main.js
+++ proman/static/main.js
@@ -1,15 +1,21 @@
-$(document).ready(function () {
-    $('card-status').mouseenter(function () {
-        $(this).animate({
-            height: '+=10px'
-        });
-    });
-    $('card-status').mouseleave(function () {
-        $(this).animate({
-            height: '-=10px'
-        });
-    });
-    $('card-status').click(function () {
-        $(this).toggle(1000);
-    });
-});
+
+$( function() {
+    $( "#status-field" ).sortable();
+    $( "#status-field" ).disableSelection();
+  } );
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+/*$('.card_content, .card_title').click(function(){
+  $(this).toggleClass('clicked');
+}*/
