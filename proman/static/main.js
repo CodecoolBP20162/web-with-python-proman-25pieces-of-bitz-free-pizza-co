b533ed15dc433291f899d5626989c6149d6ad308
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-03-28 13:11:57 +0200

    added card creation for all statuses

--- proman/static/main.js
+++ proman/static/main.js
@@ -29,9 +29,7 @@ $(document).ready(function () {
         document.getElementById("cardContent5").innerHTML = localStorage.getItem("#cardContent5");
     });
 
-    $('.add_new_card').click(function(){
-        $('#to-do .clmn-content').append('<div class="card" id="drag1" draggable="true" ondragstart="drag(event)"><header><input class="card_title" placeholder="Card Title"></header><br><article class="card_text"><textarea class="card_content" id="cardContent1" placeholder="Card Content"></textarea><button class="submit_content">Save</button> </article> </div>');
-    });
+
 
 
 });
@@ -53,3 +51,12 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+var num = 0;
+
+    function addNewCard(column){
+
+        num+=1;
+        var cardId = "drag" + num;
+        $(column+' .clmn-content').append("<div class='card' id="+cardId+" draggable='true' ondragstart='drag(event)'><header><input class='card_title' placeholder='Card Title'></header><br><article class='card_text'><textarea class='card_content' id='cardContent1' placeholder='Card Content'></textarea><button class='submit_content'>Save</button> </article> </div>");
+    };
