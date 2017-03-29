0d178c6ea9ed13bbf8a67a77344365fc5f28006f
Péter Soltész <peter.soltesz92@gmail.com>
2017-03-29 14:32:58 +0200

    saving to database WORKS

--- proman/build.py
+++ proman/build.py
@@ -6,11 +6,37 @@ class Build():
 
     @staticmethod
     def initialize_data():
-        # Schools
         board1 = Board.create(board_name='first board')
         board2 = Board.create(board_name='second board')
         board3 = Board.create(board_name='third board')
 
-        # card1 = Card.create(school_name='Codecool Budapest')
-        # card2 = Card.create(school_name='Codecool Miskolc')
-        # card3 = Card.create(school_name='Codecool Kraków')
+        card1 = Card.create(title='első',
+                            content="blabla",
+                            status="new",
+                            position=0,
+                            assigned_board=1)
+        card2 = Card.create(title='második',
+                            content="semmi",
+                            status="in_progress",
+                            position=0,
+                            assigned_board=1)
+        card3 = Card.create(title='harmadik',
+                            content="random",
+                            status="done",
+                            position=0,
+                            assigned_board=1)
+        card4 = Card.create(title='negyedik',
+                            content="ezaz",
+                            status="new",
+                            position=0,
+                            assigned_board=1)
+        card5 = Card.create(title='ötödik',
+                            content="fuck that",
+                            status="review",
+                            position=0,
+                            assigned_board=1)
+        card6 = Card.create(title='hatodik',
+                            content="pls dont",
+                            status="review",
+                            position=0,
+                            assigned_board=1)
